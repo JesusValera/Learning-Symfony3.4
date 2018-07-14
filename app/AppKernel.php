b237ae7369023f9c95a5c9da41dcd3529b6135e9
SusVa <jesus1902@outlook.com>
2018-07-15 01:41:32 +0200

    Vid 7. Annotations vs yml.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new TestBundle\TestBundle(),
+            new TestAnnotationsBundle\TestAnnotationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/TestAnnotationsBundle/TestAnnotationsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TestAnnotationsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestAnnotationsBundle extends Bundle
+{
+}
